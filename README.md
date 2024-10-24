71b63c9c2c52c18af2b3b317ab8f36964ced7cf5
Yargachin-wye <78979106+Yargachin-wye@users.noreply.github.com>
2024-10-25 02:21:23 +0700

    asmdef

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# BalisticSystem
- Moving Transform using AnimationCurve
+# Ballistic System
+
+## Moving Transform using AnimationCurve
+
+### Example:
+
+```csharp
+using Ballistic;
+using UnityEngine;
+
+public class BallisticExample : MonoBehaviour
+{
+    [SerializeField] private BallisticData ballistic;
+
+    private void FixedUpdate()
+    {
+        BallisticData.OnBallisticUpdate(ballistic, Time.fixedDeltaTime);
+    }
+}
--- script/BallisticData.cs
+++ script/BallisticData.cs
@@ -6,15 +6,51 @@ namespace Ballistic
     [Serializable]
     public class BallisticData
     {
+        /// <summary>
+        /// Начальная позиция объекта.
+        /// </summary>
         public Vector2 startPos;
+
+        /// <summary>
+        /// Конечная позиция объекта.
+        /// </summary>
         public Vector2 endPos;
+
+        /// <summary>
+        /// Трансформ объекта, который будет перемещаться.
+        /// </summary>
         public Transform transform;
+
+        /// <summary>
+        /// Начальный локальный масштаб объекта.
+        /// </summary>
         public Vector2 startLocalScale;
+
+        /// <summary>
+        /// Начальное расстояние по оси X между начальной и конечной позициями.
+        /// </summary>
         public float initialDistanceX;
+
+        /// <summary>
+        /// Общая продолжительность движения.
+        /// </summary>
         public float duration;
-        public float elapsedTime;
+
+        /// <summary>
+        /// Прошедшее время с начала движения.
+        /// </summary>
+        [HideInInspector] public float elapsedTime = 0;
+
+        /// <summary>
+        /// Кривая анимации, определяющая траекторию движения по оси Y.
+        /// </summary>
         public AnimationCurve movementCurve;
 
+        /// <summary>
+        /// Обновляет позицию и масштаб объекта на основе баллистической траектории.
+        /// </summary>
+        /// <param name="ballistic">Данные баллистической траектории.</param>
+        /// <param name="deltaTime">Время, прошедшее с последнего обновления.</param>
         public static void OnBallisticUpdate(BallisticData ballistic, float deltaTime)
         {
             float t = ballistic.elapsedTime / ballistic.duration;
